e0e3ef1d8fff26360c6d22ba05dd405647f291fe
Kuzya <alexkuz@gmail.com>
2013-03-05 22:23:45 +0400

    fix

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ BUILDDIR=$PROJDIR/bin/Release
 
 xbuild /p:Configuration=Release $PROJDIR/MonoDevelop.CoffeeScript.csproj
 
+# cp $BUILDDIR/MonoDevelop.CoffeeScript.dll build/
+
 zip -j build/MonoDevelop.CoffeeScript.mpack $BUILDDIR/MonoDevelop.CoffeeScript.dll $PROJDIR/MonoDevelop.CoffeeScript.addin.xml $PROJDIR/addin.info
